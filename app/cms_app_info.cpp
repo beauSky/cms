e2a1a83d4521ea4ec1d96312238ed5e99a0a9898
beausky <370265036@qq.com>
2019-07-21 21:42:46 +0800

    move to https://github.com/ctinkong/cms

--- app/cms_app_info.cpp
+++
@@ -1,7 +0,0 @@
-#include <app/cms_app_info.h>
-#include <string>
-
-bool	gcmsTestServer = false;
-std::string gcmsTestUrl;
-int   gcmsTestNum = 0;
-
--- enc/cms_crc32.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BASELIB_CRC32_H__
-#define __BASELIB_CRC32_H__
-#include <stdint.h>
-
-
-unsigned long CRC32(unsigned char *block, unsigned int length);
-unsigned long crc32n(unsigned char *block, unsigned int length);
-#endif
-
